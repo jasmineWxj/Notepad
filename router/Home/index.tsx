8dca301f9d238bf022f9e777738cbff34955e162
17839082108 <wxjing46@163.com>
2024-04-13 10:57:45 +0800

    feat: message

--- router/Home/index.tsx
+++ router/Home/index.tsx
@@ -1,17 +1,34 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { View, Text, Button, Alert } from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { WebView } from 'react-native-webview'
 
 const Home = () => {
+  let webViewRef = useRef() as any;
+
+  function _onH5Message(event: any) {
+    var data = JSON.stringify(event.nativeEvent.data);
+    const a = JSON.parse(event.nativeEvent.data)
+    
+    webViewRef.injectJavaScript(`receiveMessage("RN向H5发送消息");true;`)
+  }
+
+
   return (
-    <WebView
-    originWhitelist={['*']}
-    scalesPageToFit={false}
-    useWebKit={true}
-    javaScriptEnabled={true}
-    source = {{'uri':'file:///android_asset/web/index.html'}}
-    />
+    <>
+      <Text>pkpk</Text>
+      <WebView
+        ref={r => (webViewRef = r)}
+        source={require('./index.html')}
+        javaScriptEnabled={true}
+        style={{
+          flex: 1
+        }}
+        onMessage={_onH5Message}
+
+        injectedJavaScript={'onRNMessage("RN load success!")'}
+      />
+    </>
   )
 }
 
